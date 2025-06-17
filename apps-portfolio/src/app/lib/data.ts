3b5acbfdab086ea05f3c96adf3ca533b8d1c1d47
Riccardo Carlesso <palladiusbonton@gmail.com>
2025-06-17 10:39:03 +0200

    Revert "fix: Handle special characters in slugs"
    This reverts commit 00ef78bce9b4786803efc328c210faba018c301b.

--- apps-portfolio/src/app/lib/data.ts
+++ apps-portfolio/src/app/lib/data.ts
@@ -4,19 +4,8 @@ import { Talk, Article } from '../../lib/types';
 
 const slugify = (str: string) => {
   if (!str) return '';
-  const a = 'àáâäæãåāăąçćčđďèéêëēėęěğǵḧîïíīįìłḿñńǹňôöòóœøōõőṕŕřßśšşșťțûüùúūǘůűųẃẍÿýžźż·/_,:;'
-  const b = 'aaaaaaaaaacccddeeeeeeeegghiiiiiilmnnnnoooooooooprrsssssttuuuuuuuuuwxyyzzz------'
-  const p = new RegExp(a.split('').join('|'), 'g')
-
-  return str.toString().toLowerCase()
-    .replace(/\s+/g, '-') // Replace spaces with -
-    .replace(p, c => b.charAt(a.indexOf(c))) // Replace special characters
-    .replace(/&/g, '-and-') // Replace & with 'and'
-    .replace(/[^\w\-]+/g, '') // Remove all non-word chars
-    .replace(/\-\-+/g, '-') // Replace multiple - with single -
-    .replace(/^-+/, '') // Trim - from start of text
-    .replace(/-+$/, '') // Trim - from end of text
-}
+  return str.toLowerCase().replace(/[^a-z0-9]+/g, '-').replace(/(^-|-$)+/g, '');
+};
 
 const getBaseUrl = () => {
   if (process.env.NEXT_PUBLIC_API_URL) {
