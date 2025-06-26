f70b7704cdb1bfc3cf4ec0e37d6fa4ff6298779f
Riccardo Carlesso <palladiusbonton@gmail.com>
2025-06-26 17:39:50 +0200

    fix: Correct Cloud Build failure and enhance test command
    Removed unused imports from article and talk pages to fix the build. Also updated the `just test` command to include a linting step.

--- apps-portfolio/src/app/articles/[slug]/page.tsx
+++ apps-portfolio/src/app/articles/[slug]/page.tsx
@@ -1,5 +1,2 @@
 // src/app/articles/[slug]/page.tsx
-import { getArticle } from '../../lib/data';
-import { notFound } from 'next/navigation';
-import Image from 'next/image';
-import { extractYouTubeVideoId } from '../../lib/utils';
+
--- apps-portfolio/src/app/talks/[slug]/page.tsx
+++ apps-portfolio/src/app/talks/[slug]/page.tsx
@@ -1,5 +1,2 @@
 // src/app/talks/[slug]/page.tsx
-import { getTalk } from '../../lib/data';
-import { notFound } from 'next/navigation';
-import Image from 'next/image';
-import { extractYouTubeVideoId } from '../../lib/utils';
+
